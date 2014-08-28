c08eea21e43bda3b0f7f2b5c28f00325a6a6a563
laurens181 <laurensgosseling@hotmail.com>
2014-08-28 12:35:53 +0200

    Added Chart.js in resources (DO app/console update). Created routing, controller and basic view for graphs.

---
+++ src/Wecamp/Bundle/PlusplusBundle/Controller/GraphsController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Wecamp\Bundle\PlusplusBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class GraphsController extends Controller
+{
+    public function displayAction()
+    {
+        echo "bob";
+        die();
+    }
+}
---
+++ src/Wecamp/Bundle/PlusplusBundle/Tests/Controller/GraphsControllerTest.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Wecamp\Bundle\PlusplusBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class GraphsControllerTest extends WebTestCase
+{
+}
